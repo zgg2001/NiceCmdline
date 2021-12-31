e18d0ff8f70070ffac881721aae3959ca234d194
zgg2001 <397606244@qq.com>
2021-12-31 22:10:25 +0800

    宏定义文件

---
+++ lib_nice/config.h
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: config.h
+	> Author: ZHJ
+	> Remarks: NiceCmdline的宏定义配置文件
+	> Created Time: Fri 31 Dec 2021 08:51:19 PM CST
+ ************************************************************************/
+
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#define PROMPT_MAX_SIZE 32
+#define INPUT_BUF_MAX_SIZE 512
+
+#endif
