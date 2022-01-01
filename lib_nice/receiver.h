91d5dbbe85e131d064c8324bedc158fa685fcad1
zgg2001 <397606244@qq.com>
2022-01-01 21:57:45 +0800

    命令行接收器

--- lib_nice/receiver.h
+++ lib_nice/receiver.h
@@ -8,13 +8,19 @@
 #ifndef _RECEIVER_H_
 #define _RECEIVER_H_
 
-#include<lib_nice/config.h>
 #include<lib_nice/inputbuf.h>
+#include<lib_nice/parser_vt102.h>
 
 #ifdef __cplusplus
 extern "C"
 {
-#endif 
+#endif
+
+/*
+* 配置宏
+*/
+#define PROMPT_MAX_SIZE 32
+#define INPUT_BUF_MAX_SIZE 512
 
 /*
 * 接收器状态
@@ -30,15 +36,17 @@ enum receiver_status
 * 接收器结构体
 * 负责输入输出内容
 *
-* 参数一: 此接收器所有者，指向一个struct cmdline
-* 参数二: 接收器需要输出的提示符
-* 参数三: 接收器提示符的长度
-* 参数四: 接收器状态
-* 
-* 参数五: 输入缓冲区的左部分
-* 参数六: 左缓冲配套的内存
-* 参数七: 输入缓冲区的右部分
-* 参数八: 右缓冲配套的内存
+*       owner: 此接收器所有者，指向一个struct cmdline
+*      prompt: 接收器需要输出的提示符
+* prompt_size: 接收器提示符的长度
+*      status: 接收器状态
+*
+*       vt102: vt102控制码解析器
+*
+*    left_buf: 输入缓冲区的左部分
+*        left: 左缓冲配套的内存
+*   right_buf: 输入缓冲区的右部分
+*       right: 右缓冲配套的内存
 */
 struct receiver
 {
@@ -47,6 +55,8 @@ struct receiver
     char prompt[PROMPT_MAX_SIZE];
     int prompt_size;
     enum receiver_status status;
+    //控制码解析器
+    struct parser_vt102 vt102; 
     //输入缓冲区
     struct inputbuf left_buf;
     char left[INPUT_BUF_MAX_SIZE];
