faef5799178b7f5d09a4d89258d27ef80ca68a40
zgg2001 <397606244@qq.com>
2022-01-03 21:23:05 +0800

    cmdline主体

---
+++ nice_cmd/cmdline.c
@@ -0,0 +1,178 @@
+/*************************************************************************
+	> File Name: cmdline.c
+	> Author: ZHJ
+	> Remarks: cmdline相关定义 
+	> Created Time: Sun 02 Jan 2022 10:07:13 AM CST
+ ************************************************************************/
+
+/*
+ * Copyright (c) 2009, Olivier MATZ <zer0@droids-corp.org>
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the University of California, Berkeley nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include"cmdline.h"
+
+struct cmdline* 
+cmdline_get_new(const char* prompt)
+{
+	if (!prompt)
+        return NULL;
+    
+    struct cmdline *cl;
+    struct termios term;
+    
+    //cmdline内存初始化
+    cl = malloc(sizeof(struct cmdline));
+	if (cl == NULL)
+        return NULL;
+	memset(cl, 0, sizeof(struct cmdline));
+    
+    //cmdline成员初始化
+	cl->cmdline_in = INPUT_STREAM;
+	cl->cmdline_out = OUTPUT_STREAM;
+	cmdline_set_prompt(cl, prompt);
+	receiver_init(&cl->cmd_recv, cmdline_write_char);
+	cl->cmd_recv.owner = cl;
+    tcgetattr(0, &term);
+    memcpy(&cl->oldterm, &term, sizeof(struct termios));
+    
+    //终端配置设置
+	term.c_lflag &= ~(ICANON | ECHO | ISIG);
+	tcsetattr(0, TCSANOW, &term);
+	setbuf(stdin, NULL);
+    
+    //启动命令行接收器
+    receiver_new_cmdline(&cl->cmd_recv, prompt);
+    
+    return cl;
+}
+
+void
+cmdline_set_prompt(struct cmdline* cl, const char* prompt)
+{
+    if(!cl || !prompt)
+        return;
+    
+    int len;
+
+    len = strlen(prompt);
+    if(len > PROMPT_MAX_SIZE - 1)
+    {
+        const char str[] = "error: prompt length exceeds limit";
+        if(cl->cmdline_out >= 0)
+            write(cl->cmdline_out, str, strlen(str));
+        exit(1);
+    }
+    memcpy(cl->prompt, prompt, len);
+    cl->prompt[len] = '\0';
+}
+
+void
+cmdline_start_interact(struct cmdline* cl)
+{
+    if(!cl)
+        return;
+    
+    char c = -1;
+    
+    while(1)
+    {
+        if(read(cl->cmdline_in, &c, 1) <= 0)
+            break;
+        if(cmdline_parse_input(cl, &c, 1) < 0)
+            break;
+    }
+}
+
+int
+cmdline_parse_input(struct cmdline* cl, const char* buf, unsigned int size)
+{
+    if(!cl || !buf)
+        return -1;
+
+    unsigned int i;
+    int ret = -1;
+    
+    //按size对字符挨个处理
+    for(i = 0; i < size; ++i)
+    {
+        ret = receiver_parse_char(&cl->cmd_recv, buf[i]);
+    }
+    
+    return i;
+}
+
+void
+cmdline_quit(struct cmdline* cl)
+{
+    if(!cl)
+        return;
+}
+
+void
+cmdline_exit_free(struct cmdline* cl)
+{
+    if(!cl)
+        return;
+    
+    //关闭输入输出流
+	if (cl->cmdline_in > 2)
+        close(cl->cmdline_in);
+	if (cl->cmdline_out != cl->cmdline_in && cl->cmdline_out > 2)
+        close(cl->cmdline_out);
+	
+    //free并且恢复终端设置
+    free(cl);
+    tcsetattr(fileno(stdin), TCSANOW, &cl->oldterm);
+}
+
+int
+cmdline_write_char(struct receiver* recv, char c)
+{
+    if(!recv)
+        return -1;
+    
+    int ret= -1;
+    struct cmdline *cl;
+    
+    cl = recv->owner;
+    if(cl->cmdline_out >= 0)
+        ret = write(cl->cmdline_out, &c, 1);
+    
+    return ret;
+}
+
+
+
+
+
+
+
+
+
---
+++ nice_cmd/cmdline.h
@@ -0,0 +1,116 @@
+/*************************************************************************
+	> File Name: cmdline.h
+	> Author: ZHJ
+	> Remarks: NiceCmdline的最外层结构cmdline
+	> Created Time: Wed 29 Dec 2021 08:36:13 PM CST
+ ************************************************************************/
+
+/*
+ * Copyright (c) 2009, Olivier MATZ <zer0@droids-corp.org>
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the University of California, Berkeley nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _CMDLINE_H_
+#define _CMDLINE_H_
+
+#include<termios.h>
+#include<nice_cmd/receiver.h>
+
+#ifdef __cplusplus
+extern "C" 
+{
+#endif
+
+/*
+* 命令行的输入输出流
+*/
+#define INPUT_STREAM 0
+#define OUTPUT_STREAM 1
+
+/*
+* struct cmdline为最外层结构体，
+* 交互式命令行由此结构体配置
+* 其通过函数cmdline_get_new()来新建
+*
+*      prompt: 命令行提示符
+*   cmd_group: 命令行的命令组
+*    cmd_recv: 命令行的接收器
+*  cmdline_in: 输入流
+*       -- 默认设为标准输入流
+* cmdline_out: 输出流
+*       -- 默认设为标准输出流
+*     oldterm: 终端配置备份
+*       -- 退出命令行时恢复终端设置
+*/
+struct cmdline
+{
+    char prompt[PROMPT_MAX_SIZE];
+    //struct command* cmd_group;
+    struct receiver cmd_recv;
+    int cmdline_in;
+    int cmdline_out;
+    struct termios oldterm;
+};
+
+/*
+* 获取新的cmdline 
+*/
+struct cmdline* cmdline_get_new(const char* prompt);
+
+/*
+* 为指定cmdline设置提示符
+*/
+void cmdline_set_prompt(struct cmdline* cl, const char* prompt);
+
+/*
+* 指定cmdline开始交互
+*/
+void cmdline_start_interact(struct cmdline* cl);
+
+/*
+* 指定cmdline录入指定内容
+*/
+int cmdline_parse_input(struct cmdline* cl, const char* buf, unsigned int size);
+
+/*
+* 指定cmdline退出
+*/
+void cmdline_quit(struct cmdline* cl);
+
+/*
+* free指定cmdline并恢复终端设置
+*/
+void cmdline_exit_free(struct cmdline* cl);
+
+/*
+* 接收器回调函数 - 字符输出操作
+*/
+int cmdline_write_char(struct receiver* recv, char c);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
